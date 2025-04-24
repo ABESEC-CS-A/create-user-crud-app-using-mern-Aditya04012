3f0e95db23e6fdd5fdc3e697dd20d173da065703
Aditya Bhatnagar <adityabhatnagar391@gmail.com>
2025-04-24 11:51:39 +0530

    first and last commit

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Footer.css"
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <h5>BY Aditya Bhatnagar ,Copyright © 2024-25</h5>
+    </div>
+  )
+}
+
+export default Footer
---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Header.css"
+import '../App.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>CRUD APP</h1>
+    </div>
+  )
+}
+
+export default Header
